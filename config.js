5714980f7269f4a367c7a1efe37a0d1145f72d2f
kevalparikh50 <k_parikh3@fanshaweonline.ca>
2019-03-21 16:48:44 -0400

    styles done

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    user: "root",
+    password: "",
+    port: 3306, // for mac 8889
+    host: "localhost",
+    database: "marvelcharacters"
+    
+}
+
+module.exports = config;
